5e0f5830ab2f4e385d1b82e5cb1c31b2b8a3a96d
eresto <erestocorp@mail.ru>
2023-04-19 22:21:51 +0500

    Init.

---
+++ Assets/Basics/Triggers/InteractTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractTrigger : Trigger
+{
+    public string interactName;
+    public Sprite Icon;
+    
+    public virtual void Interact()
+    {
+        if (!enabled)
+        {
+            return;
+        }
+        InitTrigger();
+    }
+}
